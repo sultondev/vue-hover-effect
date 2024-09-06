b1f3562ead7b00b2c73c5817338d47a0b9ae07e5
Sultonkhon <sultondeveloper@gmail.com>
2024-09-06 22:33:53 +0500

    fix: implemented typescript support and minimized built chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 import checker from "vite-plugin-checker";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import {resolve} from 'path'
-import typescript2 from 'rollup-plugin-typescript2';
 import visualizer from "rollup-plugin-visualizer";
+// import typescript2 from 'rollup-plugin-typescript2';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode })=>({
@@ -15,26 +15,28 @@ export default defineConfig(({ command, mode })=>({
       vueTsc: true,
     }),
     cssInjectedByJsPlugin(),
-    typescript2({
-      check: false,
-      include: ["src/components/**/*.vue"],
-      tsconfigOverride: {
-        compilerOptions: {
-          outDir: "dist",
-          sourceMap: true,
-          declaration: true,
-          declarationMap: true,
-        },
-      },
-      exclude: ["vite.config.ts"]
-    })
+    // typescript2({
+    //   check: false,
+    //   include: ["src/components/**/*.vue"],
+    //   tsconfigOverride: {
+    //     compilerOptions: {
+    //       outDir: "dist-types",
+    //       sourceMap: true,
+    //       declaration: true,
+    //       declarationMap: true,
+    //     },
+    //   },
+    //   exclude: ["vite.config.ts"]
+    // })
   ],
   resolve: {
     alias: {
       src: fileURLToPath(new URL('src', import.meta.url)),
       public: fileURLToPath(new URL('public', import.meta.url)),
+      formats: ["umd", "umd"]
     },
   },
+  define: { 'process.env.NODE_ENV': '"production"' },
   build: {
     // cssCodeSplit: true,
     lib: {
@@ -42,12 +44,12 @@ export default defineConfig(({ command, mode })=>({
       name: 'VueHoverEffect',
       fileName: 'vue-hover-effect'
     },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    // minify: 'terser',
+    // terserOptions: {
+    //   compress: {
+    //     drop_console: true,
+    //   },
+    // },
     rollupOptions: {
       plugins: [
         // Only include the visualizer plugin when running in 'analyze' mode
