2a364e9b0a23e8b5ed2874ac6504a34cdb4c478a
sultondev <sultondeveloper@gmail.com>
2025-02-21 23:57:12 +0500

    fix: reconfigured everything

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+
+// 1) Export your SFC component
+export { default as SingleHoverEffect } from "./components/SingleHoverEffect.vue";
+
+// 2) Export the function + types
+export * from "./libs/hover-effect/single-hover-effect.ts";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      dts({
+        // output dir for dts files
+        entryRoot: "src",
+        outDir: "dist/types",
+        insertTypesEntry: true,
+        tsconfigPath: './tsconfig.app.json'
+      })
+  ],
+  build: {
+    lib: {
+      // Your main entry
+      entry: "src/index.ts",
+      name: "VueHoverEffect",
+      fileName: (format) => `vue-hover-effect.${format}.js`,
+    },
+    rollupOptions: {
+      // Make sure to externalize deps that shouldn't be bundled
+      external: ["vue", "three", "gsap"],
+      output: {
+        // Provide global variables to use in UMD builds for these externals
+        globals: {
+          vue: "Vue",
+          three: "THREE",
+          gsap: "gsap",
+        },
+      },
+      input: {
+        main: 'src/index.ts', // Explicitly define entry
+      },
+    },
+  }
 })
